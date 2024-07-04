edf4efab8acb1302fd535ca221920a4326685331
arosha619 <90087992+arosha619@users.noreply.github.com>
2024-07-04 14:38:15 +0530

    portfolio 1st version released

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,29 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/arosha2.jpg" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">Get to know</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            An enthusiastic and passionate individual who has excellent
+            motivation toward the success of my career with up-to-date technical
+            and management skills. My smart thinking pattern and confidence in
+            developing applications for the future industrial world help me
+            perform my role best. Hard work and commitment to work under any
+            condition lead the way to achieving personal and professional goals
+            and aspirations on time.
+          </p>
+          <p className="hero--section-description">
+            {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
+            recusandae? */}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,117 @@
+import { Typewriter } from 'react-simple-typewriter';
+
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey,👋 I'm Arosha Sandaruwan</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color"> &lt;Full Stack</span>{" "}
+            <br />
+            Developer/&gt;
+          </h1>
+          <p className="hero--section-description">
+            Fourth Year IT Undergraduate
+            <br /> at Faculty of Information Technology, University of Moratuwa.
+            <br />
+            <span style={{ fontSize: "bold", fontFamily: "italic" }}>
+              <br />
+              "I believe if we view things differently, we can create a
+              different reality"
+            </span>
+          </p>
+        </div>
+        
+        <a
+          href="https://drive.google.com/file/d/123CUV0WJl2LCMDUDTw3Gy0-eD3i2PxLx/view?usp=sharing"
+          className="btn btn-primary"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{marginTop:"10px"}}
+        >
+          Download CV
+        </a>
+        
+        <div className="hero--social--icon">
+          <ul>
+            <li>
+              <a
+                href="https://www.facebook.com/"
+                className="navbar--content"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="./img/facebook.png"
+                  alt="Facebook"
+                  width="32"
+                  height="32"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/"
+                className="navbar--content"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="./img/instagram.png"
+                  alt="insta"
+                  width="32"
+                  height="32"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.twitter.com/"
+                className="navbar--content"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="./img/twitter.png"
+                  alt="twitter"
+                  width="32"
+                  height="32"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/arosha-sandaruwan-726077213/"
+                className="navbar--content"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="./img/linkedin.png"
+                  alt="linkedin"
+                  width="32"
+                  height="32"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="hero--section--img">
+        <img src="./img/arosha.png" alt="Hero Section" />
+        <div className="logos">
+          <img src="./img/java.png" alt="JavaScript" className="logo js" />
+          <img src="./img/js.png" alt="ls" className="logo ls" />
+          <img src="./img/github.png" alt="React" className="logo react" />
+          <img src="./img/css-3.png" alt="CSS" className="logo css" />
+          <img src="./img/html-5.png" alt="HTML" className="logo html" />
+          <img src="./img/mysql.png" alt="Python" className="logo python" />
+          <img src="./img/react.png" alt="Java" className="logo java" />
+          <img src="./img/nodejs.png" alt="CSharp" className="logo csharp" />
+        </div>
+      </div>
+    </section>
+  );
+}
