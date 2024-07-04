c5f1db34d2bd00df57fce2675dbd9a3ff06f4d6b
arosha619 <90087992+arosha619@users.noreply.github.com>
2024-07-04 16:59:08 +0530

    contact page updated

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -1,82 +1,88 @@
-export default function ContactMe() {
+import React, { useRef, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import emailjs from '@emailjs/browser';
+
+const ContactMe = () => {
+  const form = useRef();
+
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_06xe7vr', 'template_e4w1x35', form.current, 'sztTgBPGNRNeyQUv5')
+      .then((result) => {
+        console.log(result,"success");
+        toast.success('Email successfully sent!');
+        form.current.reset();
+      }, (error) => {
+        console.log(error);
+        toast.error('Email sending failed, Please try again!');
+      });
+  };
+
   return (
     <section id="Contact" className="contact--section">
       <div>
         <p className="sub--title">Get In Touch</p>
         <h2>Contact Me</h2>
-        <p className="text-lg">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, odit.
-        </p>
+        <p className="text-lg">Contact me to request any service I offer...</p>
       </div>
-      <form className="contact--form--container">
+      
+      <form className="contact--form--container" ref={form} onSubmit={sendEmail}>
         <div className="container">
-          <label htmlFor="first-name" className="contact--label">
-            <span className="text-md">First Name</span>
+          <label htmlFor="user_name" className="contact--label">
+            <span className="text-md">Full Name</span>
             <input
               type="text"
               className="contact--input text-md"
-              name="first-name"
-              id="first-name"
+              name="user_name"
+              id="user_name"
               required
             />
           </label>
-          <label htmlFor="last-name" className="contact--label">
-            <span className="text-md">Last Name</span>
-            <input
-              type="text"
-              className="contact--input text-md"
-              name="last-name"
-              id="last-name"
-              required
-            />
-          </label>
+     
           <label htmlFor="email" className="contact--label">
             <span className="text-md">Email</span>
             <input
               type="email"
               className="contact--input text-md"
-              name="email"
+              name="user_email"
               id="email"
               required
             />
           </label>
-          <label htmlFor="phone-number" className="contact--label">
-            <span className="text-md">phone-number</span>
+          <label htmlFor="subject" className="contact--label">
+            <span className="text-md">Subject</span>
             <input
-              type="number"
+              type="text"
               className="contact--input text-md"
-              name="phone-number"
-              id="phone-number"
+              name="subject"
+              id="subject"
               required
             />
           </label>
         </div>
-        <label htmlFor="choode-topic" className="contact--label">
-          <span className="text-md">Choose a topic</span>
-          <select id="choose-topic" className="contact--input text-md">
-            <option>Select One...</option>
-            <option>Item 1</option>
-            <option>Item 2</option>
-            <option>Item 3</option>
-          </select>
-        </label>
         <label htmlFor="message" className="contact--label">
           <span className="text-md">Message</span>
           <textarea
             className="contact--input text-md"
             id="message"
+            name="message"
             rows="8"
             placeholder="Type your message..."
+            required
           />
         </label>
-        <label htmlFor="checkboc" className="checkbox--label">
-          <input type="checkbox" required name="checkbox" id="checkbox" />
-          <span className="text-sm">I accept the terms</span>
-        </label>
         <div>
-          <button className="btn btn-primary contact--form--btn">Submit</button>
+          <button type="submit" className="btn btn-primary contact--form--btn">
+            Submit
+          </button>
         </div>
       </form>
+      <ToastContainer />
     </section>
   );
-}
+};
+
+export default ContactMe;
