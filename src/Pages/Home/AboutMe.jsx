730aebea3e70b21aa9ed621d84e2bd1516073db5
arosha619 <90087992+arosha619@users.noreply.github.com>
2024-07-04 22:48:23 +0530

    about me section updated

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -1,29 +1,46 @@
-export default function AboutMe() {
+import React from "react";
+
+const AboutSection = () => {
   return (
-    <section id="AboutMe" className="about--section">
-      <div className="about--section--img">
-        <img src="./img/wife.jpeg" alt="About Me" />
+    <div className="about-section">
+      <div className="about-section-img">
+        <img src="./img/arosha2.jpg" alt="About Me" />
       </div>
-      <div className="hero--section--content--box about--section--box">
-        <div className="hero--section--content">
-          <p className="section--title">Get to know</p>
-          <h1 className="skills-section--heading">About Me</h1>
-          <p className="hero--section-description">
-            An enthusiastic and passionate individual who has excellent
-            motivation toward the success of my career with up-to-date technical
-            and management skills. My smart thinking pattern and confidence in
-            developing applications for the future industrial world help me
-            perform my role best. Hard work and commitment to work under any
-            condition lead the way to achieving personal and professional goals
-            and aspirations on time.
+      <div className="about-section-content">
+      <p className="section--title">Get to know</p>
+      <h2 className="skills-section--heading">About Me</h2>
+        <p>
+          An enthusiastic and passionate individual who has excellent motivation
+          toward the success of my career with up-to-date technical and
+          management skills. My smart thinking pattern and confidence in
+          developing applications for the future industrial world help me
+          perform my role best. Hard work and commitment to work under any
+          condition lead the way to achieving personal and professional goals
+          and aspirations on time.
+        </p>
+        <div className="info">
+          <p>
+            <strong>First Name:</strong> Arosha 
           </p>
-          <p className="hero--section-description">
-            {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae? */}
+          <p>
+            <strong>Last Name:</strong> Sandaruwan
+          </p>
+          <p>
+            <strong>Phone:</strong> +94 762356027
+          </p>
+          <p>
+            <strong>Languages:</strong> English, Sinhala
+          </p>
+          <p>
+            <strong>Location:</strong> Sri Lanka
+          </p>
+          <p>
+            <strong>Email:</strong> aroshasandaruwan619@gmail.com
           </p>
         </div>
       </div>
-    </section>
+    </div>
   );
-}
+};
+
+export default AboutSection;
