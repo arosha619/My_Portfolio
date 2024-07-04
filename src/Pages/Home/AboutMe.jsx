f1046fe87c11adf4aa4f8eed30d98de2e8f7b18b
arosha619 <90087992+arosha619@users.noreply.github.com>
2024-07-04 17:19:19 +0530

    test

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -2,7 +2,7 @@ export default function AboutMe() {
   return (
     <section id="AboutMe" className="about--section">
       <div className="about--section--img">
-        <img src="./img/arosha2.jpg" alt="About Me" />
+        <img src="./img/wife.jpeg" alt="About Me" />
       </div>
       <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content">
